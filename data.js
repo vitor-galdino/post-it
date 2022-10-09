0680c65099e892f6ee5287bbe719422c23be7a79
Vitor Galdino <httpvittor@gmail.com>
2022-10-08 22:02:06 -0300

    included localStorage

---
+++ data.js
@@ -0,0 +1,42 @@
+let userData = [
+    {
+        id: 1,
+        title: "Post-it",
+        text: "",
+    },
+    {
+        id: 2,
+        title: "Post-it",
+        text: "",
+    },
+    {
+        id: 3,
+        title: "Post-it",
+        text: "",
+    },
+    {
+        id: 4,
+        title: "Post-it",
+        text: "",
+    },
+    {
+        id: 5,
+        title: "Post-it",
+        text: "",
+    },
+    {
+        id: 6,
+        title: "Post-it",
+        text: "",
+    },
+    {
+        id: 7,
+        title: "Post-it",
+        text: "",
+    },
+    {
+        id: 8,
+        title: "Post-it",
+        text: "",
+    },
+];
---
+++ script.js
@@ -0,0 +1,43 @@
+const checkUserLog = () => {
+    const userLog = JSON.parse(localStorage.getItem("userSave"));
+
+    if (!userLog) {
+        renderPostIt(userData)
+    } else {
+        renderPostIt(userLog);
+        userData = userLog;
+    }
+}
+
+const renderPostIt = (obj) => {
+    const divContainer = document.querySelector(".container");
+    obj.forEach((elem) => {
+        const { id, title, text } = elem;
+
+        const div = document.createElement("div");
+        div.classList = `post cor-${id}`, div.id = id;
+        div.innerHTML = `
+        <input class="all-inputs" type="text" value="${title}" maxlength="22">
+        <textarea class="all-inputs" name="post-${id}" cols="30" rows="10">${text}</textarea>
+        `;
+        divContainer.append(div);
+    })
+}
+
+checkUserLog();
+
+const saveUserLog = () => {
+    const inputs = document.querySelectorAll(".all-inputs");
+    inputs.forEach((input) => {
+        input.oninput = (e) => {
+            const index = userData.findIndex((obj) => obj.id === parseInt(e.path[1].id));
+
+            userData[index].title = e.path[1].children[0].value;
+            userData[index].text = e.path[1].children[1].value;
+
+            const jsonUserData = JSON.stringify(userData);
+            localStorage.setItem("userSave", jsonUserData)
+        }
+    })
+}
+saveUserLog();
